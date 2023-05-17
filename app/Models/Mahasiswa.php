b135e4d703f77cc22e73079542d55a7f3da37ee3
Iemaduddin <iemaduddin17@gmail.com>
2023-05-17 09:52:21 +0700

    first commit

---
+++ app/Models/Mahasiswa.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mahasiswa extends Model
+{
+    use HasFactory;
+    protected $table = "mahasiswas"; // Eloquent akan membuat model mahasisw menyimpan record di tabel mahasiswas
+    public $timestamps = false;
+    protected $primaryKey = 'Nim'; // Memanggil isi DB Dengan primarykey
+    public $incrementing = false;
+    /**
+     * The attributes that are mass assignable.
+     * *
+     * @var array
+     */
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'featured_image',
+        'Tanggal_Lahir',
+        'kelas_id',
+        'Jurusan',
+        'No_Handphone',
+        'Email',
+    ];
+}
